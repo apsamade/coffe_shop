748aea9dd5b2aa53bd61b35296310bddf8b8d24d
apsamade <bouderga.abdessamade@gmail.com>
2023-06-09 01:25:22 +0200

    maj

--- public/js/navBurger.js
+++ public/js/navBurger.js
@@ -6,10 +6,14 @@ monBurger.addEventListener('click', ()=>{
     console.log('ok')
     if(monBurger.classList.contains('croix')){
         monBurger.classList.remove('croix')
+        monBurger.classList.add('visible')
+    
         maAside.style.right = '-301px';
         
     }else{
         monBurger.classList.add('croix')
+        monBurger.classList.remove('visible')
+        
         maAside.style.right = '0';
         
     }
