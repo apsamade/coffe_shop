38d7444b1e386e3c471dd5a81155b708d69b9cb0
apsamade <bouderga.abdessamade@gmail.com>
2023-06-09 22:52:50 +0200

    maj

--- public/js/defile.js
+++ public/js/defile.js
@@ -6,51 +6,30 @@ document.addEventListener("DOMContentLoaded", function() {
     var slideIndex = 0;
     var slideWidth = slides[0].offsetWidth;
     var slideT = 0;
-    
-    // Vérifie si l'article 1 est entièrement visible dans la fenêtre
-    function isArticle1Visible() {
-      var article1Rect = slides[0].getBoundingClientRect();
-      return (article1Rect.left >= 0 && article1Rect.right <= window.innerWidth);
-    }
-    
-    // Vérifie si le dernier article est entièrement visible dans la fenêtre
-    function isLastArticleVisible() {
-      var lastArticleRect = slides[slides.length - 1].getBoundingClientRect();
-      return (lastArticleRect.left >= 0 && lastArticleRect.right <= window.innerWidth);
-    }
-    
     prevButton.addEventListener("click", prevSlide);
     nextButton.addEventListener("click", nextSlide);
-    
+  
     function prevSlide() {
-      if (isArticle1Visible()) {
-        return; // Ne fait rien si l'article 1 est entièrement visible
-      }
-      
       slideIndex--;
       slideT += slideWidth;
+      if (slideIndex < 0) {
+        slideIndex = slides.length - 2;
+        slideT = -slideWidth * slideIndex;
+      }
       updateSlide();
     }
-    
+  
     function nextSlide() {
-      if (isLastArticleVisible()) {
-        return; // Ne fait rien si le dernier article est entièrement visible
-      }
-      
       slideIndex++;
       slideT -= slideWidth;
+      if (slideIndex >= slides.length - 1) {
+        slideIndex = 0;
+        slideT = 0;
+      }
       updateSlide();
     }
-    
-    function updateSlide() {
-        slider.style.transform = "translateX(" + slideT + "px)";
-        
-        if (slideIndex === slides.length - 1) {
-          slider.classList.add("last-slide");
-        } else {
-          slider.classList.remove("last-slide");
-        }
-      }
-      
-  });
   
+    function updateSlide() {
+      slider.style.transform = "translateX(" + slideT + "px)";
+    }
+});
