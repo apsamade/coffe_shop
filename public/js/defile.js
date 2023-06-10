f7dec55a347267a5a227e7a5b130bf30762aafd7
apsamade <bouderga.abdessamade@gmail.com>
2023-06-10 17:37:16 +0200

    maj

--- public/js/defile.js
+++ public/js/defile.js
@@ -1,34 +1,76 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var slides = document.querySelectorAll(".article");
-    var prevButton = document.querySelector(".prev");
-    var nextButton = document.querySelector(".next");
-    var slideWidth = slides[0].offsetWidth;
-    let slideArray = []
+var articlesSection = document.querySelector('.articles');
+var articles = articlesSection.getElementsByClassName('article');
+var currentIndex = 0;
 
-    
+function handleSlider() {
+  var windowWidth = window.innerWidth;
+  var firstArticle = articles[0];
+  var rect = firstArticle.getBoundingClientRect();
 
-    slides.forEach((div, index)=>{
-      div.setAttribute('data-order', index + 1)
-      slideArray.push(div)
-    })
+  if (rect.left >= 0) {
+    prevButton.disabled = true; // Désactiver le bouton de défilement vers la gauche au démarrage
+  } else {
+    prevButton.disabled = false; // Activer le bouton de défilement vers la gauche au démarrage
+  }
 
-    function moveLastToFirst() {
-      const lastElement = slideArray.pop();
-      slideArray.unshift(lastElement);
+  for (var i = 0; i < articles.length; i++) {
+    var article = articles[i];
+    var rect = article.getBoundingClientRect();
+
+    // Vérifie si l'élément sort de la fenêtre
+    if (rect.right > windowWidth) {
+      // L'élément est en dehors de la fenêtre, vous pouvez implémenter le code pour le faire défiler ici
+      console.log("L'élément ", i, " sort de la fenêtre !");
     }
-    function moveFirstToLast() {
-      const firstElement = slideArray.shift();
-      slideArray.push(firstElement);
-    }
-    
-    prevButton.addEventListener("click", ()=>{
-      moveFirstToLast();
-      console.log(slideArray)
-    });
-    nextButton.addEventListener("click", ()=>{
-      moveLastToFirst();
-      console.log(slideArray)
-    });
+  }
+}
 
-    
-});
+function scrollLeft() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = 0;
+  }
+  articlesSection.style.transition = 'transform 0.3s ease-in-out';
+  articlesSection.style.transform = `translateX(-${currentIndex * 371}px)`;
+
+
+  var firstArticle = articles[0];
+  var rect = firstArticle.getBoundingClientRect();
+
+  if (rect.left >= 0) {
+    prevButton.disabled = true; 
+  } else {
+    prevButton.disabled = false; 
+  }
+
+  nextButton.disabled = false;
+}
+
+function scrollRight() {
+  currentIndex++;
+  if (currentIndex >= articles.length) {
+    currentIndex = articles.length - 1;
+  }
+  articlesSection.style.transition = 'transform 0.3s ease-in-out';
+  articlesSection.style.transform = `translateX(-${currentIndex * 481}px)`;
+
+  var lastArticle = articles[articles.length - 2];
+  var rect = lastArticle.getBoundingClientRect();
+  var windowWidth = window.innerWidth;
+
+  if (rect.right <= windowWidth) {
+    nextButton.disabled = true; 
+  } else {
+    nextButton.disabled = false; 
+  }
+
+  prevButton.disabled = false; 
+}
+
+document.addEventListener('DOMContentLoaded', handleSlider);
+
+var prevButton = document.querySelector('.prev');
+var nextButton = document.querySelector('.next');
+
+prevButton.addEventListener('click', scrollLeft);
+nextButton.addEventListener('click', scrollRight);
